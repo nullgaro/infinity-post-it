9416b61df0338f31720439b7b9fdd1e3d902b384
Daniel Milenov <danimileniv@gmail.com>
2023-06-24 02:12:16 +0200

    Tailwind is working

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,17 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import "./app.css";
 </script>
 
-<template lang="pug">
-  div
-    a(href='https://vitejs.dev' target='_blank' rel='noreferrer')
-      img.logo(src='{viteLogo}' alt='Vite Logo')
-    a(href='https://svelte.dev' target='_blank' rel='noreferrer')
-      img.logo.svelte(src='{svelteLogo}' alt='Svelte Logo')
-  h1 Vite + Svelte
-  .card
-    Counter
-  p
-    | Check out
-    a(href='https://github.com/sveltejs/kit#readme' target='_blank' rel='noreferrer') SvelteKit
-    | , the official Svelte app framework powered by Vite!
-  p.read-the-docs
-    | Click on the Vite and Svelte logos to learn more
+<template lang="pug" class="underline">
+  div#navbar
+    h1.text-3xl.font-bold.underline Infinity Post-it
+    a#navlink(href="https://google.com") About
+    button
+      img(src="#" alt="test")
 
 </template>
 
-<style lang="sass">
-  .logo
-    height: 6em
-    padding: 1.5em
-    will-change: filter
-    transition: filter 300ms
-
-  .logo:hover
-    filter: drop-shadow(0 0 2em #646cffaa)
-
-  .logo.svelte:hover
-    filter: drop-shadow(0 0 2em #ff3e00aa)
-
-  .read-the-docs
-    color: #888
-
+<style lang="sass" scoped>
+  #navbar
+    display: flex
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import preprocess from 'svelte-preprocess';
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+
   preprocess:  preprocess({
     pug: true
-  })
+  }),
 }
