cdad666fc1b4b5cb22454bd86e1598207a62fc04
Daniel Milenov <danimileniv@gmail.com>
2023-07-05 01:50:58 +0200

    Fix class props of ProfileIcon

--- src/App.svelte
+++ src/App.svelte
@@ -4,19 +4,19 @@
 </script>
 
 <template lang="pug">
-  div(class="bg-p-white h-screen flex flex-col items-center")
+  div(class="bg-p-light-gray h-screen flex flex-col items-center")
     div(class="w-full py-4 px-6 bg-p-gray flex place-content-between")
       h1(class="text-4xl text-p-white") Infinity Post-it
       div(class="w-40 flex justify-around place-content-center")
         a(href="/about" class="mt-2 text-p-white align-baseline duration-150 hover:text-p-navy") About
         button
-          ProfileIcon(size="32px" color="white" class="[&>*]:duration-150 [&>*]:hover:stroke-p-navy")
+          ProfileIcon(class="h-8 w-8 [&>*]:stroke-p-white [&>*]:duration-150 [&>*]:hover:stroke-p-navy")
 
     div(class="h-2/6 w-4/5 sm:w-[30rem] mt-20 bg-p-gray rounded-lg flex flex-col items-center justify-center")
       form(class="h-full w-11/12 flex flex-col justify-evenly")
         div(class="w-full flex justify-center items-center")
           div(class="h-1/5 w-1/2 flex items-center")
-            ProfileIcon(size="40px" color="white" class="mr-2")
+            ProfileIcon(class="h-10 w-10 mr-2 [&>*]:stroke-p-white")
             p(class="text-p-white") Annonymous
           div(class="h-full w-1/2 flex items-center justify-end")
             input(name="post" value="Post-it!" type="submit" class="h-8 w-3/6 rounded-2xl bg-p-navy")
