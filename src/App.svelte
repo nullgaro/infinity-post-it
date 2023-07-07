f290c31d389a784ed0d1aef9642a356d7e9787dc
Daniel Milenov <danimileniv@gmail.com>
2023-07-07 16:38:51 +0200

    Restructuring and checking for responsive

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <template lang="pug">
-  div(class="bg-p-light-gray h-screen flex flex-col items-center")
+  div(class="bg-p-light-gray flex flex-col items-center")
     div(class="w-full py-4 px-6 bg-p-gray flex place-content-between")
       h1(class="text-4xl text-p-white") Infinity Post-it
       div(class="w-40 flex justify-around place-content-center")
@@ -14,7 +14,7 @@
         button
           ProfileIcon(class="h-8 w-8 [&>*]:stroke-p-white [&>*]:duration-150 [&>*]:hover:stroke-p-navy")
 
-    div(class="h-2/6 w-4/5 sm:w-[30rem] mt-20 bg-p-gray rounded-lg flex flex-col items-center justify-center")
+    div(class="h-96 w-4/5 sm:w-[30rem] my-20 bg-p-gray rounded-lg flex flex-col items-center justify-center")
       form(class="h-full w-11/12 flex flex-col justify-evenly")
         div(class="w-full flex justify-center items-center")
           div(class="h-full w-1/2 flex items-center gap-3")
@@ -24,14 +24,14 @@
             input(name="post" value="Post-it!" type="submit" class="h-8 w-3/6 rounded-2xl bg-p-navy border border-solid border-p-gray transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-p-light-navy hover:border hover:border-solid hover:border-p-navy hover:shadow-[0_0_6px_0_rgba(0,173,181,1)]")
         textarea(placeholder="Type your Post-it..." class="h-4/6 w-full p-3 rounded-md bg-p-white resize-none outline-none border-2 border-solid border-p-gray focus:border-2 focus:border-solid focus:border-p-navy transition-all duration-300 ease-in-out focus:shadow-[0_0_6px_0_rgba(0,173,181,1)]")
 
-    div(class="h-52 w-full bg-p-brown mt-52 ")
-      div(class="w-full py-4 px-6 bg-p-gray flex place-content-end gap-5")
-          button
-            //- https://www.svgrepo.com/svg/510981/filter
-            FilterIcon(class="h-8 w-8 [&>*>*]:stroke-p-white [&>*>*]:duration-150 [&>*>*]:hover:stroke-p-navy")
-          button
-            //- https://www.svgrepo.com/svg/364195/arrows-down-up-fill
-            OrderIcon(class="h-8 w-8 [&>*]:fill-p-white [&>*]:duration-150 [&>*]:hover:fill-p-navy")
+  div(class="h-52 w-full bg-p-brown")
+    div(class="w-full py-4 px-6 bg-p-gray flex place-content-end gap-5")
+        button
+          //- https://www.svgrepo.com/svg/510981/filter
+          FilterIcon(class="h-8 w-8 [&>*>*]:stroke-p-white [&>*>*]:duration-150 [&>*>*]:hover:stroke-p-navy")
+        button
+          //- https://www.svgrepo.com/svg/364195/arrows-down-up-fill
+          OrderIcon(class="h-8 w-8 [&>*]:fill-p-white [&>*]:duration-150 [&>*]:hover:fill-p-navy")
 </template>
 
 <style lang="sass" scoped>
