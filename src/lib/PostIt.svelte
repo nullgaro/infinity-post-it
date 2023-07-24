1b7137a7944c388349b0190029fe3d948dd51fea
Daniel Milenov <danimileniv@gmail.com>
2023-07-24 21:06:45 +0200

    Add new component PostIt

---
+++ src/lib/PostIt.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+
+    let ProfileIcon;
+    // let className;
+    // export { className as class };
+</script>
+<template lang="pug">
+    li(class="bg-yellow-600 text-p-white block h-52 w-52 p-4 m-4 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]")
+        h2(class="font-bold text-xl") Title #1
+        p(class="text-sm") Text Content #1
+</template>
