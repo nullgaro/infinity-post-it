4877e08fc26c0770461f1bb07119ec8d23b319a2
Daniel Milenov <danimileniv@gmail.com>
2023-07-24 21:07:40 +0200

    Add prototype of corkboard with Post-It's

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import ProfileIcon from "./lib/ProfileIcon.svelte";
   import FilterIcon from "./lib/FilterIcon.svelte";
   import OrderIcon from "./lib/OrderIcon.svelte";
+  import PostIt from "./lib/PostIt.svelte";
 </script>
 
 <template lang="pug">
@@ -19,12 +20,12 @@
         div(class="w-full flex justify-center items-center")
           div(class="h-full w-1/2 flex items-center gap-3")
             ProfileIcon(class="h-10 w-10 [&>*]:stroke-p-white")
-            p(class="text-p-white") Annonymous
+            p(class="text-p-white") Anonymous
           div(class="h-full w-1/2 flex items-center justify-end")
             input(name="post" value="Post-it!" type="submit" class="h-8 w-3/6 rounded-2xl bg-p-navy border border-solid border-p-gray transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-p-light-navy hover:border hover:border-solid hover:border-p-navy hover:shadow-[0_0_6px_0_rgba(0,173,181,1)]")
         textarea(placeholder="Type your Post-it..." class="h-4/6 w-full p-3 rounded-md bg-p-white resize-none outline-none border-2 border-solid border-p-gray focus:border-2 focus:border-solid focus:border-p-navy transition-all duration-300 ease-in-out focus:shadow-[0_0_6px_0_rgba(0,173,181,1)]")
 
-  div(class="h-52 w-full bg-p-brown")
+  div(class="w-full bg-p-brown")
     div(class="w-full py-4 px-6 bg-p-gray flex place-content-end gap-5")
         button
           //- https://www.svgrepo.com/svg/510981/filter
@@ -32,6 +33,17 @@
         button
           //- https://www.svgrepo.com/svg/364195/arrows-down-up-fill
           OrderIcon(class="h-8 w-8 [&>*]:fill-p-white [&>*]:duration-150 [&>*]:hover:fill-p-navy")
+    ul(class="grid grid-cols-7 grid-flow-row justify-start")
+      PostIt
+      PostIt
+      PostIt
+      PostIt
+      PostIt
+      PostIt
+      PostIt
+      PostIt
+      PostIt
+
 </template>
 
 <style lang="sass" scoped>
