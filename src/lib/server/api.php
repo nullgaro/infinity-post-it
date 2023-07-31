4b8ef4bc407e9ec7fbaee9d50a03fc070815147e
Daniel Milenov <danimileniv@gmail.com>
2023-07-31 19:54:31 +0200

    Finish api.php (renamed from Form.php)

--- src/lib/server/api.php
+++ src/lib/server/api.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 spl_autoload_register(function ($class) {
     require __DIR__ . "/db/$class.php";
+    require __DIR__ . "/Exceptions/$class.php";
+    require __DIR__ . "/Gateway/$class.php";
+    require __DIR__ . "/Controller/$class.php";
 });
 
+// Dotenv
 require 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
 
 header("Content-type: application/json; charset=UTF-8");
 
@@ -21,13 +28,15 @@ if ($parts[1] != "post-its") {
 
 $id = $parts[2] ?? null;
 
+$host = $_ENV["DB_HOST"];
+$db = $_ENV["DB_DATABASE"];
 $user = $_ENV["DB_NAME"];
 $pass = $_ENV["DB_PASS"];
 
-$database = new Database("localhost", "postit_db", "$user", "$pass");
+$database = new Database("$host", "$db", "$user", "$pass");
 
-$database -> getConnection();
+$gateway = new PostitGateway($database);
 
-$controller = new PostitController;
+$controller = new PostitController($gateway);
 
 $controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
