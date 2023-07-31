c7be9ad44b096a44cddf3273e24d2466a3a82808
Daniel Milenov <danimileniv@gmail.com>
2023-07-31 19:55:40 +0200

    Finish Database.php

--- src/lib/server/db/Database.php
+++ src/lib/server/db/Database.php
@@ -11,6 +11,9 @@ Class Database {
     public function getConnection(): PDO {
         $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
 
-        return new PDO($dsn, $this->user, $this->password);
+        return new PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false
+        ]);
     }
 }
