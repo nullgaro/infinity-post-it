a1f735b303100066f02db4aef6eb42be2f4871a6
Daniel Milenov <danimileniv@gmail.com>
2023-08-03 18:58:30 +0200

    Create processCollectionRequest()

--- src/lib/server/Controller/UserController.php
+++ src/lib/server/Controller/UserController.php
@@ -33,6 +33,34 @@ class UserController {
         }
     }
 
+    private function processCollectionRequest(string $method): void {
+        switch ($method) {
+            case "POST":
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+
+                $errors = $this->getValidateionErrors($data);
+
+                if(! empty($errors)) {
+                    http_response_code(422);
+                    echo json_encode(["errors" => $errors]);
+                    break;
+                }
+
+                $id = $this->gateway->create($data);
+
+                http_response_code(201);
+                echo json_encode([
+                    "message" => "User created",
+                    "id" => $id
+                ]);
+                break;
+
+            default:
+                http_response_code(405);
+                header("Allow: POST");
+        }
+    }
+
     // TODO: Modify errors
     private function getValidateionErrors(array $data): array {
         $errors = [];
