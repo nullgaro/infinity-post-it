3e8724fc3630abb501709d1255de8c98bd0828e2
Daniel Milenov <danimileniv@gmail.com>
2023-08-19 19:46:14 +0200

    Add profile picture to post-it

--- src/lib/PostIt.svelte
+++ src/lib/PostIt.svelte
@@ -3,9 +3,13 @@
 
     let ProfileIcon;
     export let props;
+    let profilePath = `./images/profiles/${props.author.toLowerCase()}.png`
+    console.log(profilePath)
 </script>
 <template lang="pug">
     li(class="bg-yellow-600 text-p-white block h-52 w-52 p-4 m-4 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]")
-        h2(class="font-bold text-xl") {props.author}
+        div(class="flex w-full h-1/6 justify-start mb-2")
+            img(src="{profilePath}" alt="{props.author}" class="h-full mr-2")
+            h2(class="font-bold text-xl") {props.author}
         p(class="text-sm") {props.content}
 </template>
