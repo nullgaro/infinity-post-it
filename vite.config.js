c91e833d0b94e27ee37049f870cf790c9071c344
Daniel Milenov <danimileniv@gmail.com>
2023-08-23 18:13:13 +0200

    Allow public/images in vite server

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['./public/images'],
+    },
+  },
 })
