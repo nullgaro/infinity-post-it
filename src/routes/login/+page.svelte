121b5ed17323d84522d535c7bdec8926cdfd8508
Daniel Milenov <danimileniv@gmail.com>
2023-08-24 21:25:48 +0200

    Create basic login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+  import "../../app.css";
+  import ProfileIcon from "../../lib/ProfileIcon.svelte";
+  import FilterIcon from "../../lib/FilterIcon.svelte";
+  import OrderIcon from "../../lib/OrderIcon.svelte";
+  import PostIt from "../../lib/PostIt.svelte";
+
+  const login = (async (e) => {
+		// const formData = new FormData(e.target)
+		// let data = {}
+
+		// for (let field of formData) {
+    //   const [key, value] = field
+		// 	data[key] = value
+		// }
+
+    // const response = await fetch("http://localhost:8080/post-its", {
+    //   method: "POST",
+    //   body: JSON.stringify(data),
+    //   headers: {"Content-type": "application/json; charset=UTF-8"}
+    // });
+
+    // if(response.status) {
+    //   window.location.reload(true);
+    // }
+
+  });
+
+</script>
+
+<template lang="pug">
+  div(class="min-h-screen w-full bg-p-light-gray flex flex-col items-center justify-between")
+    div(class="w-full py-4 px-6 bg-p-gray flex place-content-start")
+      a(href="/")
+        h1(class="text-4xl text-p-white") Infinity Post-it
+
+    div(class="h-96 w-4/5 sm:w-[30rem] my-20 bg-p-gray rounded-lg flex flex-col items-center justify-center gap-5")
+      h2(class="text-p-white text-3xl") Login
+      form(on:submit|preventDefault="{login}" class="h-2/4 w-11/12 flex flex-col justify-evenly items-center")
+        input(name="username" type="text" class="h-8 w-4/6 rounded-md bg-p-white border border-solid border-p-gray")
+        input(name="password" type="password" class="h-8 w-4/6 rounded-md bg-p-white border border-solid border-p-gray")
+        input(name="post" value="Login" type="submit" class="h-8 w-4/6 rounded-2xl bg-p-navy border border-solid border-p-gray transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-p-light-navy hover:border hover:border-solid hover:border-p-navy hover:shadow-[0_0_6px_0_rgba(0,173,181,1)]")
+
+    div(class="w-full py-4 px-6 bg-p-gray flex place-content-end")
+</template>
+
+<style lang="sass" scoped>
+</style>
