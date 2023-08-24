36d31f2344d09075923b2e80a75afc67c3f78c3d
Daniel Milenov <danimileniv@gmail.com>
2023-08-24 18:47:25 +0200

    Change login button to an anchor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,9 +38,9 @@
     div(class="bg-p-light-gray flex flex-col items-center")
       div(class="w-full py-4 px-6 bg-p-gray flex place-content-between")
         h1(class="text-4xl text-p-white") Infinity Post-it
-        div(class="w-40 flex justify-around place-content-center")
-          a(href="/about" class="mt-2 text-p-white align-baseline duration-150 hover:text-p-navy") About
-          button
+        div(class="w-40 flex justify-around items-center")
+          a(href="/about" class="text-p-white align-baseline duration-150 hover:text-p-navy") About
+          a(href="/login")
             ProfileIcon(class="h-8 w-8 [&>*]:stroke-p-white [&>*]:duration-150 [&>*]:hover:stroke-p-navy")
 
       div(class="h-96 w-4/5 sm:w-[30rem] my-20 bg-p-gray rounded-lg flex flex-col items-center justify-center")
