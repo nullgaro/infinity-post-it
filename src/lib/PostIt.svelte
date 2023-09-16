82e611b21618a3c960ae1027451e9e1ef623d293
Daniel Milenov <danimileniv@gmail.com>
2023-09-16 23:28:24 +0200

    Make username case sensitive

--- src/lib/PostIt.svelte
+++ src/lib/PostIt.svelte
@@ -3,7 +3,7 @@
 
     let ProfileIcon;
     export let props;
-    let profilePath = `./public/images/profiles/${props.author.toLowerCase()}.png`
+    let profilePath = `./public/images/profiles/${props.author}.png`
 </script>
 <template lang="pug">
     li(class="bg-yellow-600 text-p-white block h-52 w-52 p-4 m-4 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]")
